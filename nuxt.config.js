7983e4942b354897795f30a76b58f8d1fa073086
dhirusarania <dhirusarania@gmail.com>
2020-02-10 19:06:24 +0530

    sdsd

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,96 +1,97 @@
 export default {
-  mode: "universal",
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || "",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href: "/css/bootstrap.min.css"
-      },
-      {
-        rel: "stylesheet",
-        href: "/css/font-awesome.css"
-      },
-      {
-        rel: "stylesheet",
-        href: "/css/framework.css"
-      },
-      {
-        rel: "stylesheet",
-        href: "https://cdn.plyr.io/3.5.6/plyr.css"
-      }
-    ],
-    script: [
-      {
-        src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
-        type: "text/javascript"
-      },
-      {
-        src:
-          "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
-        type: "text/javascript"
-      },
-      {
-        src: "/js/bootstrap.min.js",
-        type: "text/javascript"
-      }
-    ]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-    { src: '~/plugins/carousel', lang: 'js', ssr: false },
-  ],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/axios",
-    "@nuxtjs/pwa", 
-    ['nuxt-gmaps', {
-      key: 'AIzaSyDs4ugbl-tlSSUh22cvkxx_XhcB-eFstcU',
-      //you can use libraries: ['places']
-    }]
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
-  /*
-   ** Build configuration
-   */
-  build: {
+    mode: "universal",
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
+    // server: {     
+    //   port: 8000, // default: 3000     
+    //   host: '0.0.0.0', // default: localhost   
+    // },
+    head: {
+        title: process.env.npm_package_name || "",
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            {
+                hid: "description",
+                name: "description",
+                content: process.env.npm_package_description || ""
+            }
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            {
+                rel: "stylesheet",
+                href: "/css/bootstrap.min.css"
+            },
+            {
+                rel: "stylesheet",
+                href: "/css/font-awesome.css"
+            },
+            {
+                rel: "stylesheet",
+                href: "/css/framework.css"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://cdn.plyr.io/3.5.6/plyr.css"
+            }
+        ],
+        script: [{
+                src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
+                type: "text/javascript"
+            },
+            {
+                src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+                type: "text/javascript"
+            },
+            {
+                src: "/js/bootstrap.min.js",
+                type: "text/javascript"
+            }
+        ]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: "#fff" },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        { src: '~/plugins/carousel', lang: 'js', ssr: false },
+    ],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        "@nuxtjs/axios",
+        "@nuxtjs/pwa", ['nuxt-gmaps', {
+            key: 'AIzaSyDs4ugbl-tlSSUh22cvkxx_XhcB-eFstcU',
+            //you can use libraries: ['places']
+        }]
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {},
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 };
