862ec23ada1d52d9026eb2100f3e0758b98ca5a2
unknown <chiranjibpaul007@gmail.com>
2020-03-06 19:22:08 +0530

    changes

--- pages/pknetwork.vue
+++ pages/pknetwork.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-      <Breadcrumbs title="PK Networks" subtitle="" crumbs="Home > PK Networks"></Breadcrumbs>
-    <div class="container">
+    <Breadcrumbs title="PK Networks" subtitle crumbs="Home > PK Networks"></Breadcrumbs>
+    <div class="container-small">
+      <div style="padding-top: 40px">
+        <div class="w-100">
+          <div>
+            <img class="coma" src="/images/asset1.png" alt />
+          </div>
+          <div class="container">
+            <p>PK Security has a deeply connected, truly vast network that ensures people receive consistent, high-quality, distinctive, world-class service. We are one of India's largest, most awarded, integrated security services agency. We’ll work with you to assess your security and a holistic approach to other needs and structure our services accordingly. Just reach out to any of our key office contacts below and we will help you with uninterrupted service.</p>
+          </div>
+          <div class="d-flex justify-content-end">
+            <img style="right:100%" class="coma" src="/images/asset3.png" alt />
+          </div>
+        </div>
+      </div>
       <div class="d-flex justify-content-center my-5">
         <img class="w-75" src="/images/PK_MAP1.jpg" alt />
       </div>
